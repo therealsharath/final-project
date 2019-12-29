98e43f68163845c911b6aa360400e0e527c0ecec
Sharath Palathingal <palathingal@gatech.edu>
2019-12-29 15:04:05 -0500

    Updated Application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar'
+import Section from './components/Section'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="buffer"></div>
     </div>
   );
 }
